b5b00209060e47c0ecb9ed0c605f8c1b4f7e8af7
Filip <>
2025-02-19 14:54:04 +0100

    init

---
+++ src/consts.ts
@@ -0,0 +1,25 @@
+import type { Metadata, Site, Socials } from "@types";
+
+export const SITE: Site = {
+  TITLE: "Miku World",
+  DESCRIPTION: "Losowe informacje o różnych vocaloidowych producentach",
+  EMAIL: "kontakt@mikuworld.pl",
+  NUM_POSTS_ON_HOMEPAGE: 5
+};
+
+export const HOME: Metadata = {
+  TITLE: "Strona Główna",
+  DESCRIPTION: "Losowe informacje o różnych vocaloidowych producentach",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "Losowe informacje o różnych vocaloidowych producentach",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "Twitter",
+    HREF: "https://twitter.com/mikuworldpl",
+  }
+];
